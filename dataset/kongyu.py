64355f383c1b2d1d1090a5a6b0472fde9d22aaf3
Nicolezzh <86824356+Nicole998@users.noreply.github.com>
2024-11-17 18:01:05 +0800

    Update kongyu.py

--- dataset/kongyu.py
+++ dataset/kongyu.py
@@ -2,8 +2,8 @@ from .kybase import *
 
 class KongYu(MyDataset):
     def __init__(self, root,  mode, transform = None):
-        self.data =  '/root/autodl-tmp/data/201116-X_all(label_noise_remove_0.3,size3605).npy'
-        self.labels = '/root/autodl-tmp/data/201116-Y_all(label_noise_remove_0.3,size3605).npy'
+        self.data =  '/root/autodl-tmp/data/data.npy'
+        self.labels = '/root/autodl-tmp/data/lable.npy'
         self.mode = mode
         self.transform = transform
     
